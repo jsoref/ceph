ab62de602ca031c5519a83b6b568535a93fe97c3
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-22 23:17:35 -0400

    spelling: synchronous
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/libradosstriper/RadosStriperImpl.h
+++ src/libradosstriper/RadosStriperImpl.h
@@ -108,7 +108,7 @@ struct RadosStriperImpl {
 
   // asynchronous remove. Note that the removal is not 100% parallelized :
   // the removal of the first rados object of the striped object will be
-  // done via a syncrhonous call after the completion of all other removals.
+  // done via a synchronous call after the completion of all other removals.
   // These are done asynchronously and in parallel
   int aio_remove(const std::string& soid, librados::AioCompletionImpl *c, int flags=0);
 
