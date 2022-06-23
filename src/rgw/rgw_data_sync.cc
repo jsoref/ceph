0a287cc862541ced2c2061ad513acc84b8002ce6
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-22 22:13:11 -0400

    spelling: incremental
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -3687,7 +3687,7 @@ public:
    * create index from key -> <op, marker>, and from marker -> key
    * this is useful so that we can insure that we only have one
    * entry for any key that is used. This is needed when doing
-   * incremenatl sync of data, and we don't want to run multiple
+   * incremental sync of data, and we don't want to run multiple
    * concurrent sync operations for the same bucket shard 
    * Also, we should make sure that we don't run concurrent operations on the same key with
    * different ops.
