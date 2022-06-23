a773f5fea1547c9929e35659d43846ab80485bd3
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-22 22:35:23 -0400

    spelling: nonsense
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/python-common/ceph/tests/test_disk_selector.py
+++ src/python-common/ceph/tests/test_disk_selector.py
@@ -123,19 +123,19 @@ class TestSizeMatcher(object):
         assert matcher.low[1] == 'GB'
 
     def test_to_byte_GB(self):
-        """ Pretty nonesense test.."""
+        """ Pretty nonsense test.."""
 
         ret = drive_selection.SizeMatcher('size', '10G').to_byte(('10', 'GB'))
         assert ret == 10 * 1e+9
 
     def test_to_byte_MB(self):
-        """ Pretty nonesense test.."""
+        """ Pretty nonsense test.."""
 
         ret = drive_selection.SizeMatcher('size', '10M').to_byte(('10', 'MB'))
         assert ret == 10 * 1e+6
 
     def test_to_byte_TB(self):
-        """ Pretty nonesense test.."""
+        """ Pretty nonsense test.."""
 
         ret = drive_selection.SizeMatcher('size', '10T').to_byte(('10', 'TB'))
         assert ret == 10 * 1e+12
